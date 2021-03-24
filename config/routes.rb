4c56f0f209228e9716e45a96ec22572d5af8efe6
fatmakhaledosman <fatma1khaled1@gmail.com>
2021-03-24 16:59:18 +0200

    add resources articles and edit path of link

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   #get 'articles/index'
-  get "/articles", to: "articles#index"
+  #get "/articles", to: "articles#index"
 
   root "articles#index"
 
-  get "/articles/:id", to: "articles#show"
-
+  #get "/articles/:id", to: "articles#show"
+  resources :articles
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
